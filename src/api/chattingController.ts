4eaf7fbc62d0043c72a8e5b60e484f25880b01e1
allburov <allburov@gmail.com>
2020-10-21 11:09:16 +0700

    Chat functions - part 1

--- src/api/chattingController.ts
+++ src/api/chattingController.ts
@@ -1,19 +1,23 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {Body, Controller, Inject, Post} from '@nestjs/common';
 import {ApiOperation, ApiTags} from "@nestjs/swagger";
 import {MessageOut, MessageReply} from "./all.dto";
+import {Whatsapp} from "venom-bot";
 
 @Controller('api')
 @ApiTags('chatting')
 export class ChattingController {
+    constructor(@Inject('WHATSAPP') private whatsapp: Whatsapp) {
+    }
+
     @Post('/sendText')
     @ApiOperation({summary: 'Send a text message'})
     sendText(@Body() message: MessageOut) {
-        return `You've sent: ${message.chatId} - ${message.text}`
+        return this.whatsapp.sendText(message.chatId, message.text)
     }
 
     @Post('/reply')
     @ApiOperation({summary: 'Reply to a text message'})
     reply(@Body() message: MessageReply) {
-        return `You've sent: ${message.chatId} - ${message.text} - ${message.reply_to}`
+        return this.whatsapp.reply(message.chatId, message.text, message.reply_to, [])
     }
 }
