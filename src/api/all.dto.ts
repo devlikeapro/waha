4b9bac77f0660263e9f1d1aa326f17eac879134b
allburov <allburov@gmail.com>
2020-10-22 10:20:59 +0700

    Add chatId examples

--- src/api/all.dto.ts
+++ src/api/all.dto.ts
@@ -1,25 +1,39 @@
+import {ApiProperty} from "@nestjs/swagger";
+
+const chatId = ApiProperty({
+    example: '791231234567@c.us'
+})
+
 export class Chat {
+    @chatId
     chatId: string;
 }
 
 export class MessageContactVcard {
+    @chatId
     chatId: string;
     contactsId: string;
     name: string
 }
 
 export class MessageText {
+    @chatId
     chatId: string;
     text: string;
 }
 
 export class MessageReply {
+    @chatId
     chatId: string;
     text: string;
+    @ApiProperty({
+        example: 'message.id',
+    })
     reply_to: string;
 }
 
 export class MessageLocation {
+    @chatId
     chatId: string;
     latitude: string;
     longitude: string;
@@ -27,6 +41,7 @@ export class MessageLocation {
 }
 
 export class MessageImage {
+    @chatId
     chatId: string;
     path: string;
     filename: string;
@@ -34,6 +49,7 @@ export class MessageImage {
 }
 
 export class MessageFile {
+    @chatId
     chatId: string;
     path: string;
     filename: string;
@@ -41,6 +57,7 @@ export class MessageFile {
 }
 
 export class MessageLinkPreview {
+    @chatId
     chatId: string;
     url: string;
     title: string;
