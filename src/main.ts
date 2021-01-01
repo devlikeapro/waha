dcf4b956800acfd7c8200eef49a2ddf376ece0d4
allburov <allburov@gmail.com>
2021-01-01 23:37:45 +0700

    Add github link to swagger

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     app.enableShutdownHooks();
     const options = new DocumentBuilder()
         .setTitle('WhatsApp HTTP API')
-        .setDescription('WhatsApp HTTP API that you can configure in a click')
+        .setDescription('WhatsApp HTTP API that you can configure in a click!\nGithub: https://github.com/allburov/whatsapp-http-api')
         .setVersion('1.0')
         .addTag('device', 'Device information')
         .addTag('chatting', 'Chat methods')
