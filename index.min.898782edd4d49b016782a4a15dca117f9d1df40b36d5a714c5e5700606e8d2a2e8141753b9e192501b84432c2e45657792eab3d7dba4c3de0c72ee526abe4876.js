018a87e68fd9c9f12833fb315f0927c1bd4e0412
allburov <allburov@gmail.com>
2023-07-16 09:53:12 +0000

    Deploying to gh-pages from @ devlikeapro/whatsapp-http-api@ef96fbf8c572b769a98b3ff7610def9504ef550d 🚀

--- index.min.898782edd4d49b016782a4a15dca117f9d1df40b36d5a714c5e5700606e8d2a2e8141753b9e192501b84432c2e45657792eab3d7dba4c3de0c72ee526abe4876.js
+++ index.min.898782edd4d49b016782a4a15dca117f9d1df40b36d5a714c5e5700606e8d2a2e8141753b9e192501b84432c2e45657792eab3d7dba4c3de0c72ee526abe4876.js
@@ -126,9 +126,9 @@ WHATSAPP_RESTART_ALL_SESSIONS=True: Set this variable to True to start all STOPP
 Parameters in path that you can find in below endpoints:
 {session} - use the session name for Whatsapp instance that you created with POST /api/session/start endpoint {chatId} - chat id in format 123123123123@[c.us|g.us], c.us for direct chats and g.us for groups. Methods #Get all chats #Get all chats GET /api/{session}/chats
 Get messages from chat #Get 100 messages from the chat
-GET /api/{session}/chats/{chatId/messages?limit=100
+GET /api/{session}/chats/{chatId}/messages?limit=100
 Get 100 messages from the chat, skip downloading media (images, files)
-GET /api/{session}/chats/{chatId/messages?limit=100\u0026amp;downloadMedia=false
+GET /api/{session}/chats/{chatId}/messages?limit=100\u0026amp;downloadMedia=false
 Delete chat #Use the method to delete chat
 DELETE /api/{session}/chats/{chatId}
 Clear messages #Use the method to clear all messages from the chat
