96f29823252c4792505472f82282a9fe244301f0
allburov <allburov@gmail.com>
2023-08-23 12:15:06 +0300

    [core] Bump to 2023.8.23

--- src/version.ts
+++ src/version.ts
@@ -26,7 +26,7 @@ export function getWAHAVersion(): WAHAVersion {
 }
 
 export const VERSION = {
-  version: '2023.8.21',
+  version: '2023.8.23',
   tier: getWAHAVersion(),
   browser: getBrowserExecutablePath(),
 };
