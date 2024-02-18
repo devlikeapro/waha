a14c702c791e982f069d0dbbfbc3eb962cc68215
allburov <allburov@gmail.com>
2024-02-18 19:25:12 +0700

    [core] Bump to 2024.2.3

--- src/version.ts
+++ src/version.ts
@@ -33,7 +33,7 @@ export function getEngineName(): string {
 }
 
 export const VERSION: WAHAEnvironment = {
-  version: '2023.12.3',
+  version: '2024.2.3',
   engine: getEngineName(),
   tier: getWAHAVersion(),
   browser: getBrowserExecutablePath(),
