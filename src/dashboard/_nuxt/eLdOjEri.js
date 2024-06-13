87b24e35d0853d76acdfd4a7edb97526487bf992
devlikepro <waha@devlike.pro>
2024-06-13 15:58:37 +0700

    [core] update dashboard

---
+++ src/dashboard/_nuxt/eLdOjEri.js
@@ -0,0 +1,1 @@
+import{ax as r}from"./CdAotn7S.js";var e=r();export{e as O};
---
+++ src/dashboard/_nuxt/fmRlOMwp.js
@@ -0,0 +1,1 @@
+import r from"./uEiA7KRH.js";import{P as t,t as o}from"./CdAotn7S.js";import"./B4IxZQIs.js";import"./DiD3_pIL.js";const i={__name:"AppSidebar",setup(e){return(p,a)=>(o(),t(r))}};export{i as default};
