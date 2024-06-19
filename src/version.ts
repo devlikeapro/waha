6e9fe42a83048eb9816f87b76e08436c69b422a9
devlikepro <waha@devlike.pro>
2024-06-19 12:55:14 +0700

    [core] bump version

--- src/version.ts
+++ src/version.ts
@@ -33,7 +33,7 @@ export function getEngineName(): string {
 }
 
 export const VERSION: WAHAEnvironment = {
-  version: '2024.6.5',
+  version: '2024.6.6',
   engine: getEngineName(),
   tier: getWAHAVersion(),
   browser: getBrowserExecutablePath(),
