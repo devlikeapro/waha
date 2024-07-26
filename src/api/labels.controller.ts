ce9f91fe2c1cc50095c913e9cf92c3a3a436510f
devlikepro <waha@devlike.pro>
2024-07-26 19:43:57 +0300

    [core] Use /api/{session}/labels

--- src/api/labels.controller.ts
+++ src/api/labels.controller.ts
@@ -1,20 +1,21 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ApiOperation, ApiSecurity, ApiTags } from '@nestjs/swagger';
+import { SessionApiParam, SessionParam } from '@waha/api/helpers';
+import { WhatsappSession } from '@waha/core/abc/session.abc';
 import { Label } from '@waha/structures/labels.dto';
 
 import { SessionManager } from '../core/abc/manager.abc';
-import { SessionQuery } from '../structures/base.dto';
 
 @ApiSecurity('api_key')
-@Controller('api/labels')
+@Controller('api/:session/labels')
 @ApiTags('labels')
 export class LabelsController {
   constructor(private manager: SessionManager) {}
 
   @Get('/')
+  @SessionApiParam
   @ApiOperation({ summary: 'Get all labels' })
-  getAll(@Query() query: SessionQuery): Promise<Label[]> {
-    const whatsapp = this.manager.getSession(query.session);
-    return whatsapp.getLabels();
+  getAll(@SessionParam session: WhatsappSession): Promise<Label[]> {
+    return session.getLabels();
   }
 }
