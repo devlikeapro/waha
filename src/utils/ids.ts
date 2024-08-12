1163dee35260002feb0d69ec6a47589ff20fb474
devlikepro <waha@devlike.pro>
2024-08-12 10:21:45 +0700

    [core] uuid4

--- src/utils/ids.ts
+++ src/utils/ids.ts
@@ -1,4 +1,10 @@
+import { v4 as uuid4 } from 'uuid';
+
+/**
+ * Generate prefix uuid (but remove -)
+ * @param prefix
+ */
 export function generatePrefixedId(prefix: string) {
-  const id = Math.random().toString(36).substring(7);
+  const id = uuid4().replace(/-/g, '');
   return `${prefix}_${id}`;
 }
